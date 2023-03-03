4f4333e29f97a8a0dfecb2fc215de55745d71ba7
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-03 22:39:35 +0900

    feat: Member entity 추가

--- ninetynine/src/main/java/com/example/ninetynine/domain/member/entity/Member.java
+++ ninetynine/src/main/java/com/example/ninetynine/domain/member/entity/Member.java
@@ -1,12 +1,16 @@
 package com.example.ninetynine.domain.member.entity;
 
 import com.example.ninetynine.domain.common.entity.Timestamped;
+import com.example.ninetynine.domain.photo.entity.Photo;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member extends Timestamped {
 
     @Id
@@ -28,8 +32,7 @@ public class Member extends Timestamped {
     @Column
     private String profilePic;
 
-
-
-
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Photo> photos = new ArrayList<>();
 
 }
