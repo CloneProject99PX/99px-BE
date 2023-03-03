00c27e2d9acb0de1eadcbb0b458c1f097f67b0eb
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-04 00:45:15 +0900

    feat: Address enum 추가

---
+++ ninetynine/src/main/java/com/example/ninetynine/domain/common/entity/Address.java
@@ -0,0 +1,328 @@
+package com.example.ninetynine.domain.common.entity;
+
+public enum Address {
+
+    SEOUL,
+
+    BUSAN,
+
+    INCHON,
+
+    DAEGU,
+
+    DAEJEON,
+
+    KWANGJU,
+
+    SUWON,
+
+    ULSAN,
+
+    GOYANG,
+
+    YONGIN,
+
+    CHANGWON,
+
+    SEONGNAM,
+
+    CHEONG_JU,
+
+    BUCHON,
+
+    HWASEONG,
+
+    NAMYANGJU,
+
+    JEONJU,
+
+    CHEONAN,
+
+    ANSAN,
+
+    ANYANG,
+
+    KIMHAE,
+
+    PYEONGTAEK,
+
+    POHANG,
+
+    JEJU,
+
+    SIHEUNG,
+
+    PAJU,
+
+    UIJEONGBU,
+
+    GIMPO,
+
+    GUMI,
+
+    GWANGJU,
+
+    YANGSAN,
+
+    WONJU,
+
+    JINJU,
+
+    SEJONG,
+
+    KWANGMYUNG,
+
+    ASAN,
+
+    IKSAN,
+
+    CHUNCHEON,
+
+    KYUNGSAN,
+
+    GUNPO,
+
+    GUNSAN,
+
+    HANAM,
+
+    YEOSU,
+
+    SUNCHEON,
+
+    KYUNGJU,
+
+    GEOJE,
+
+    MOKPO,
+
+    OSAN,
+
+    ICHEON,
+
+    GANGNEUNG,
+
+    YANGJU,
+
+    CHUNGJU,
+
+    ANSEONG,
+
+    GURI,
+
+    SEOSAN,
+
+    SEOGWIPO,
+
+    DANGJIN,
+
+    ANDONG,
+
+    POCHEON,
+
+    UIWANG,
+
+    GWANGYANG,
+
+    GIMCHEON,
+
+    JECHEON,
+
+    TONGYEONG,
+
+    NONSAN,
+
+    CHILGOK,
+
+    SACHEON,
+
+    YEOJU,
+
+    GONGJU,
+
+    YANGPYEONG,
+
+    JEONGEUP,
+
+    YEONGJU,
+
+    NAJU,
+
+    EUMSEONG,
+
+    MILYANG,
+
+    HONGSEONG,
+
+    BORYEONG,
+
+    WANJU,
+
+    SANGJU,
+
+    YEONGCHEON,
+
+    DONGDUCHEON,
+
+    DONGHAE,
+
+    GIMJE,
+
+    MUAN,
+
+    NAMWON,
+
+    JINCHEON,
+
+    YESAN,
+
+    SOKCHO,
+
+    MUNGYEONG,
+
+    HAMAN,
+
+    SAMCHEOK,
+
+    HONGCHEON,
+
+    HAENAM,
+
+    BUYEO,
+
+    CHANGNYEONG,
+
+    TAEAN,
+
+    GOHEUNG,
+
+    HWASUN,
+
+    GEOCHANG,
+
+    GAPYEONG,
+
+    YEONGAM,
+
+    GEUMSAN,
+
+    GOCHANG,
+
+    GWACHEON,
+
+    SEOCHEON,
+
+    GOSEONG,
+
+    BUAN,
+
+    UISEONG,
+
+    OKCHEON,
+
+    YEONGGWANG,
+
+    YEONGDONG,
+
+    ULJIN,
+
+    WANDO,
+
+    YECHON,
+
+    CHEOLWON,
+
+    TAEBAEK,
+
+    YEONCHON,
+
+    DAMYANG,
+
+    HAPCHEON,
+
+    HADONG,
+
+    HOENGSEONG,
+
+    NAMHAE,
+
+    KYERYONG,
+
+    JANGSEONG,
+
+    CHEONGDO,
+
+    SEONGJU,
+
+    PYEONGCHANG,
+
+    BOSEONG,
+
+    GOESAN,
+
+    HAMYANG,
+
+    JEONGPYEONG,
+
+    YEONGWOL,
+
+    JANGHEUNG,
+
+    YEONGDEOK,
+
+    JEONGSEON,
+
+    SHINAN,
+
+    SANCHEONG,
+
+    GANGJIN,
+
+    GORYUNG,
+
+    BOEUN,
+
+    CHEONGYANG,
+
+    BONGHWA,
+
+    HAMPYEONG,
+
+    INJE,
+
+    JINDO,
+
+    GOKSEONG,
+
+    GOSEUNG,
+
+    DANYANG,
+
+    SUNCHANG,
+
+    IMSIL,
+
+    UIRYUNG,
+
+    YANGYANG,
+
+    HWACHEON,
+
+    CHEONGSONG,
+
+    GURYE,
+
+    MUSU,
+
+    JINAN,
+
+    YANGGU,
+
+    GUNWI,
+
+    JANGSU,
+
+    YEONGYANG,
+
+    ULLEUNG
+}
