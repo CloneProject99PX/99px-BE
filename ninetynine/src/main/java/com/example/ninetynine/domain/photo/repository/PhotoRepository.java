cbc3bf484ed2e85d6a9471ddba1dd551bb6c23d4
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-07 20:51:11 +0900

    feat: PhotoRepository에 로직 일부 추가

--- ninetynine/src/main/java/com/example/ninetynine/domain/photo/repository/PhotoRepository.java
+++ ninetynine/src/main/java/com/example/ninetynine/domain/photo/repository/PhotoRepository.java
@@ -1,7 +1,17 @@
 package com.example.ninetynine.domain.photo.repository;
 
+import com.example.ninetynine.domain.common.entity.Category;
 import com.example.ninetynine.domain.photo.entity.Photo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    @Modifying
+    @Query("update Photo p set p.view = p.view+1 where p.id = :id")
+    int updateView(Long id);
+
+    Slice<Photo> findByCategory(Category category, Pageable pageable);
 }
