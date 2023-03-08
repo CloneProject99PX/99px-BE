45236faac6f694ac42f3a549de10e7e3932a5465
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-09 01:57:23 +0900

    feat: MemberResponseDto작업

--- ninetynine/src/main/java/com/example/ninetynine/domain/photo/dto/PhotoResponseDto.java
+++ ninetynine/src/main/java/com/example/ninetynine/domain/photo/dto/PhotoResponseDto.java
@@ -1,7 +1,8 @@
 package com.example.ninetynine.domain.photo.dto;
 
-import com.example.ninetynine.domain.common.entity.Location;
+import com.example.ninetynine.api.dto.MemberResponseDto;
 import com.example.ninetynine.domain.common.entity.Category;
+import com.example.ninetynine.domain.common.entity.Location;
 import com.example.ninetynine.domain.photo.entity.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,13 +36,18 @@ public class PhotoResponseDto {
 
     private Long view;
 
+    private MemberResponseDto memberResponseDto;
+
+
     public PhotoResponseDto(Photo photo){
         this.id = photo.getId();
+        this.title= photo.getTitle();
         this.url = photo.getUrl();
         this.description = photo.getDescription();
         this.location = photo.getLocation();
         this.category = photo.getCategory();
         this.nsfw = photo.isNsfw();
         this.view = photo.getView();
+        this.memberResponseDto = new MemberResponseDto(photo.getMember());
     }
 }
