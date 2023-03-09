cd7d35f21c3576ee35b7e11dcf2e56c74f56bfbf
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-09 17:48:41 +0900

    chore: 사용하지 않는 import문 제거

--- ninetynine/src/main/java/com/example/ninetynine/global/error/GlobalExceptionHandler.java
+++ ninetynine/src/main/java/com/example/ninetynine/global/error/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.example.ninetynine.global.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
